3724387ef8473fda24ee4f82f2418284b4e06e3c
JLW <jamie.williams@virtuallaunch.pro>
2026-03-31 10:29:14 -0700

    Changes to be committed: deleted:    .next/BUILD_ID modified:   .next/build-manifest.json modified:   .next/cache/.tsbuildinfo modified:   .next/diagnostics/build-diagnostics.json ... modified:   wrangler.toml

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,15 +7,15 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/03~yq9q893hmn.js"
   ],
   "lowPriorityFiles": [
-    "static/ZA5W7wn0ovKRdwh2oB0CQ/_buildManifest.js",
-    "static/ZA5W7wn0ovKRdwh2oB0CQ/_ssgManifest.js",
-    "static/ZA5W7wn0ovKRdwh2oB0CQ/_clientMiddlewareManifest.js"
+    "static/YpLTK5dKvqAsznWL9Ecby/_buildManifest.js",
+    "static/YpLTK5dKvqAsznWL9Ecby/_ssgManifest.js",
+    "static/YpLTK5dKvqAsznWL9Ecby/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/0m4ty02_satn~.js",
     "static/chunks/04tbl4nape-54.js",
-    "static/chunks/0wngqqki5mnhj.js",
+    "static/chunks/05lyrsalwe4c6.js",
     "static/chunks/12ldke8cs5avj.js",
-    "static/chunks/turbopack-088a2e9d8yqww.js"
+    "static/chunks/turbopack-0jxc9dj.cjvew.js"
   ]
 };
